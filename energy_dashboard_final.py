3938504da1312753b56442981c94575513417f90
mikagoyal <102133580+mikagoyal@users.noreply.github.com>
2025-07-05 02:27:02 +0530

    Source button change

--- energy_dashboard_final.py
+++ energy_dashboard_final.py
@@ -1742,7 +1742,7 @@ def main():
         if 'selected_sources' not in st.session_state:
             st.session_state.selected_sources = []
 
-        col1, col2 = st.columns([0.9, 0.1])
+        col1, col2 = st.columns([0.8, 0.2])
 
         with col1:
             st.markdown("### Latest News")
